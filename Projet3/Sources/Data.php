5fb8a6e499f1352c503cbb9e462000637b3c7df8
Komlav <116752822+Komlav@users.noreply.github.com>
2022-11-24 18:21:21 +0000

    Add files via upload

---
+++ Projet3/Sources/Data.php
@@ -0,0 +1,78 @@
+<?php
+    $AUTEURS = [
+        ["CodeAuteur" => "AUT001", "NomAuteur" => "SENGHOR", "PrénomAuteur" => "Léopold Sédar ", "Profession" => "Ecrivain"],
+        ["CodeAuteur" => "AUT002", "NomAuteur" => "BETI", "PrénomAuteur" => "Mongo", "Profession" => "Ecrivain"],
+        ["CodeAuteur" => "AUT003", "NomAuteur" => "LAYE", "PrénomAuteur" => "Camara", "Profession" => "Ecrivain"],
+        ["CodeAuteur" => "AUT004", "NomAuteur" => "CHINUA", "PrénomAuteur" => "Achebé", "Profession" => "Ecrivain, Romancier et poète"],
+        ["CodeAuteur" => "AUT005", "NomAuteur" => "BA", "PrénomAuteur" => "Amadou Hampaté", "Profession" => "Ecrivain"],
+        ["CodeAuteur" => "AUT006", "NomAuteur" => "KANE", "PrénomAuteur" => "Cheick Hamidou", "Profession" => "Ecrivain, haut fonctionnaire"],
+        ["CodeAuteur" => "AUT007", "NomAuteur" => "DIOP", "PrénomAuteur" => "David", "Profession" => "Ecrivain,enseignant-chercheur "],
+        ["CodeAuteur" => "AUT008", "NomAuteur" => "DIOP", "PrénomAuteur" => "Cheick Anta", "Profession" => "Ecrivain, scientifique de formation"],
+        ["CodeAuteur" => "AUT009", "NomAuteur" => "SEBENE", "PrénomAuteur" => "Ousmane", "Profession" => "Ecrivain, réalisateur"],
+        ["CodeAuteur" => "AUT010", "NomAuteur" => "OYONO", "PrénomAuteur" => "Ferdinand Léopold", "Profession" => "Ecrivain,  ambassadeur"]
+    ];
+
+    $OUVRAGES = [
+        ["CodeOuvrage" => 1, "Titre" => "Une Vie De Boy", "DateEdition" => 1956],
+        ["CodeOuvrage" => 2, "Titre" => "Le Vieux Nègre Et La Médaille", "DateEdition" => 1956],
+        ["CodeOuvrage" => 3, "Titre" => "Chemin d'Europe", "DateEdition" => 1960],
+        ["CodeOuvrage" => 4, "Titre" => "Les Bouts De Bois De Dieu", "DateEdition" => 1960],
+        ["CodeOuvrage" => 5, "Titre" => "Xola", "DateEdition" => 1973],
+        ["CodeOuvrage" => 6, "Titre" => "Tribals Scares", "DateEdition" => 1962],
+        ["CodeOuvrage" => 7, "Titre" => "Le Docker Noire", "DateEdition" => 1956],
+        ["CodeOuvrage" => 8, "Titre" => "Civilisation Ou Barbarie", "DateEdition" => 1981],
+        ["CodeOuvrage" => 9, "Titre" => "Nations Nègres Et Culture", "DateEdition" => 1979],
+        ["CodeOuvrage" => 10, "Titre" => "Precolonial Black Africa", "DateEdition" => 1979],
+        ["CodeOuvrage" => 11, "Titre" => "Frère d'Ame", "DateEdition" => 2018],
+        ["CodeOuvrage" => 12, "Titre" => "La Porte Du Voyage Sans Retour", "DateEdition" => 2021],
+        ["CodeOuvrage" => 13, "Titre" => "Reise Ohne Wiederk", "DateEdition" => 2021],
+        ["CodeOuvrage" => 14, "Titre" => "Les Gardiens Du Temple", "DateEdition" => 1995],
+        ["CodeOuvrage" => 15, "Titre" => "L'aventure Ambigue", "DateEdition" => 1961],
+        ["CodeOuvrage" => 16, "Titre" => "L'enfant Noire", "DateEdition" => 1953],
+        ["CodeOuvrage" => 17, "Titre" => "Le Regard Du Roi", "DateEdition" => 1954],
+        ["CodeOuvrage" => 18, "Titre" => "The Guaridan Of The Word", "DateEdition" => 1980],
+        ["CodeOuvrage" => 18, "Titre" => "The Guaridan Of The Word", "DateEdition" => 1980],
+        ["CodeOuvrage" => 19, "Titre" => "Beyond Hunger in Africa: Conventional Wisdom", "DateEdition" => 1990],
+        ["CodeOuvrage" => 20, "Titre" => "The Short Century: Independence and Liberation Of Movement", "DateEdition" => 2001]
+    ];
+
+    $EXEMPLAIRES = [
+        ["CodeExplre" => 1, "DateEnre" => 1991],
+        ["CodeExplre" => 2, "DateEnre" => 1956],
+        ["CodeExplre" => 3, "DateEnre" => 1954],
+        ["CodeExplre" => 4, "DateEnre" => 1981],
+        ["CodeExplre" => 5, "DateEnre" => 1985],
+        ["CodeExplre" => 6, "DateEnre" => 1995],
+        ["CodeExplre" => 7, "DateEnre" => 1992],
+        ["CodeExplre" => 8, "DateEnre" => 2001],
+        ["CodeExplre" => 9, "DateEnre" => 2004],
+        ["CodeExplre" => 10, "DateEnre" => 1925]
+    ];
+
+    $PRET = [
+        ["CodePret" => 1, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "L'enfant Noire", "idAdhérent" => 2],
+        ["CodePret" => 2, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Le Regard Du Roi", "idAdhérent" => 8],
+        ["CodePret" => 3, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "The Guardian Of The Word", "idAdhérent" => 4],
+        ["CodePret" => 4, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "The Short Century: Independence and Liberation Of Movement", "idAdhérent" => 7],
+        ["CodePret" => 5, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Civilisation Ou Barbarie", "idAdhérent" => 9],
+        ["CodePret" => 6, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Nations Nègres Et Culture", "idAdhérent" => 3],
+        ["CodePret" => 7, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Le Docker Noire", "idAdhérent" => 1],
+        ["CodePret" => 8, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Xola", "idAdhérent" => 6],
+        ["CodePret" => 9, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Frère d'Ame", "idAdhérent" => 10],
+        ["CodePret" => 10, "Date" => "", "DateRetour" => "", "DateRéel" => "", "Exemplaire" => "Une Vie De Boy", "idAdhérent" => 5]
+    ];
+
+    $ADHERENTS = [
+        ["IdAdérent" => 1, "NomAdhé" => "TAMAKLOE", "PrénomAdhé" => "Komlavi Nutekpor", "Téléphone" => 781077144, "Adresse" => "Niarry Tally Rue GD 12 Dakar Sénégal"],
+        ["IdAdérent" => 2, "NomAdhé" => "ANDZAYE", "PrénomAdhé" => "Divine Anaclette", "Téléphone" => 767851240, "Adresse" => "Dieupeul, derrière le commissariat"],
+        ["IdAdérent" => 3, "NomAdhé" => "BALARABE", "PrénomAdhé" => "Mariama", "Téléphone" => 785201240, "Adresse" => "Fass Paillote à coté de la pharmacie"],
+        ["IdAdérent" => 4, "NomAdhé" => "AWUME", "PrénomAdhé" => "Kelly Jason", "Téléphone" => 778521456, "Adresse" => "Mermos Immeuble Mamoune"],
+        ["IdAdérent" => 5, "NomAdhé" => "MISSEKPE", "PrénomAdhé" => "Tonton Fulbert", "Téléphone" => 708551456, "Adresse" => "Mermos Immeuble Mamoune"],
+        ["IdAdérent" => 6, "NomAdhé" => "CAMARA", "PrénomAdhé" => "Maimouna", "Téléphone" => 708551456, "Adresse" => "Liberté 4 non loin du rond point"],
+        ["IdAdérent" => 7, "NomAdhé" => "NEGLO", "PrénomAdhé" => "Kekeli Christelle", "Téléphone" => 785652888, "Adresse" => "Grand Dakar"],
+        ["IdAdérent" => 8, "NomAdhé" => "NDOUR", "PrénomAdhé" => "Salimata", "Téléphone" => 757544588, "Adresse" => "Dakar Plateau"],
+        ["IdAdérent" => 9, "NomAdhé" => "BOP", "PrénomAdhé" => "Aïcha", "Téléphone" => 780754588, "Adresse" => "Liberté 6"],
+        ["IdAdérent" => 10, "NomAdhé" => "BOISSY", "PrénomAdhé" => "Audrey Yassira", "Téléphone" => 775481268, "Adresse" => "Grand Yoff non loin de l'aéroport"],
+    ];
+
+?>
